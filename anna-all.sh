b5b8d37b96a2854eafa25c047960b44b2b1a9ed2
Jingyu Liu <jyliu00@gmail.com>
2016-07-18 22:33:24 -0700

    change

--- anna-all.sh
+++ anna-all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+start_time=`date +%H:%M:%S`
+
 for g in usa ibd biotech 3x china canada
 do
     anna -group=$g $1
 done
+
+end_time=`date +%H:%M:%S`
+
+echo "time: $start_time ~ $end_time"
