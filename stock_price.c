38b9f61b3092de8cdc31ca31445436eac33c3ae0
Jingyu Liu <jyliu00@gmail.com>
2016-11-24 22:16:28 -0800

    change

--- stock_price.c
+++ stock_price.c
@@ -1327,6 +1327,9 @@ static void symbol_check_weeks_low_sma(const char *symbol, const struct stock_pr
 		if (!prev->sma[sma2check])
 			return;
 
+		if (price2check->open < prev->sma[sma2check] && price2check->close > prev->sma[sma2check])
+			break;
+
 		if (price2check->close <= prev->sma[sma2check] || prev->close > prev->sma[sma2check])
 			return;
 
