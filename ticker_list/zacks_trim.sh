18b42966a07375188d1011492f194bb60e2590c3
Jingyu Liu <jyliu00@gmail.com>
2016-12-26 11:19:54 -0800

    update zacks_1n2.txt

--- ticker_list/zacks_trim.sh
+++ ticker_list/zacks_trim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cut -d$'\t' -f3,4,5 $1 | sed 'n; d' > zacks_1n2.txt
+cut -d$'\t' -f3,4,5,10,11,12 $1 | sed 'n; d' > zacks_1n2.txt
 
 #cut -d$'\t' -f3 $1 > .trim.zacks
 #sed 'n; d' .trim.zacks > $1
