c28127d65e94efaa77fbc7fdac22f2edf7c752a8
Jingyu Liu <jyliu@fortinet.com>
2017-01-11 11:26:27 -0800

    change anna-check.sh

--- anna-check.sh
+++ anna-check.sh
@@ -8,8 +8,8 @@ printf "\n\x1b[33mPotential Double Bottom Up\x1b[0m:\n\n"
 
 anna -group=$1 check-dbup
 
-printf "\n\x1b[33mPotential Breakout\x1b[0m:\n\n"
+printf "\n\x1b[33mUp cross SMA20\x1b[0m:\n\n"
 
-anna -group=$1 check-bo
+anna -group=$1 check-20dup
 
 echo ""
