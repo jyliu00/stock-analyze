0eee54187d086537b947a848ccf0104ba5e54cb6
Jingyu Liu <jyliu00@gmail.com>
2017-01-13 22:46:24 -0800

    update

--- anna-check.sh
+++ anna-check.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-printf "\n\x1b[33mPotential Double Bottom\x1b[0m:\n\n"
-
-anna -group=$1 check-db
-
 printf "\n\x1b[33mPotential Double Bottom Up\x1b[0m:\n\n"
 
 anna -group=$1 check-dbup
@@ -11,5 +7,9 @@ anna -group=$1 check-dbup
 printf "\n\x1b[33mUp cross SMA20\x1b[0m:\n\n"
 
 anna -group=$1 check-20dup
+
+printf "\n\x1b[33mUp cross SMA50\x1b[0m:\n\n"
+
+anna -group=$1 check-50dup
 
 echo ""
