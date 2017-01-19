690a35e46826d92093be51be9863ed5996c040a4
Jingyu Liu <jyliu@fortinet.com>
2017-01-19 14:19:36 -0800

    add zacks_2A/B/C/D/F.txt

--- ticker_list/zacks_trim.sh
+++ ticker_list/zacks_trim.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #cut -d$'\t' -f3,4,5,8,10,11,12 $1 | sed 'n; d' > $2
-cut -d$'\t' -f3,4,5,6,7 $1 | sed 'n; d' > $2
+echo "#sector=rank_$2" > zacks_$2.txt
+cut -d$'\t' -f3,4,5,6,7 $1 | sed 'n; d' >> zacks_$2.txt
 rm -f $1
 
 #cut -d$'\t' -f3 $1 > .trim.zacks
