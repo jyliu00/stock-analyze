3261e2acc37e05eef82c16f287d451554421f65b
Jingyu Liu <jyliu00@gmail.com>
2017-01-22 00:37:02 -0800

    update

--- anna-check.sh
+++ anna-check.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-printf "\n\x1b[33mDouble Bottom Up\x1b[0m:\n\n"
-
-anna -group=$1 check-dbup
-
 printf "\n\x1b[33mTrend Breakout\x1b[0m:\n\n"
 
 anna -group=$1 check-trend-bo
 
-printf "\n\x1b[33mUp cross SMA20\x1b[0m:\n\n"
+printf "\n\x1b[33mExtreme Volume at Support\x1b[0m:\n\n"
 
-anna -group=$1 check-20dup
-
-printf "\n\x1b[33mUp cross SMA50\x1b[0m:\n\n"
-
-anna -group=$1 check-50dup
+anna -group=$1 check-volume-spt
 
 echo ""
