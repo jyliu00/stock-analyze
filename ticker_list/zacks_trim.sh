15b50218cc7e2cbda43259aa95b8ba352febd652
Jingyu Liu <jyliu@fortinet.com>
2017-01-26 11:34:24 -0800

    sort zacks_1/2.txt

--- ticker_list/zacks_trim.sh
+++ ticker_list/zacks_trim.sh
@@ -3,6 +3,8 @@
 #cut -d$'\t' -f3,4,5,8,10,11,12 $1 | sed 'n; d' > $2
 echo "%sector=rank_$2" > zacks_$2.txt
 cut -d$'\t' -f3,4,5,6,7,11 $1 | sed 'n; d' >> zacks_$2.txt
+sort -k 6 zacks_$2.txt > 1
+mv 1 zacks_$2.txt
 rm -f $1
 
 #cut -d$'\t' -f3 $1 > .trim.zacks
