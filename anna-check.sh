dcb5de79ca91b5f82f8f9aedc52a32c4973a3066
Jingyu Liu <jyliu00@gmail.com>
2017-02-02 20:18:53 -0800

    update anna-check.sh

--- anna-check.sh
+++ anna-check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-printf "\n\x1b[33mBreak Out\x1b[0m:\n\n"
+printf "\n\x1b[33mStrong Break Out\x1b[0m:\n\n"
 
-anna -group=$1 check-bo
+anna -group=$1 check-strong-bo
 
 printf "\n\x1b[33mTrend Breakout\x1b[0m:\n\n"
 
