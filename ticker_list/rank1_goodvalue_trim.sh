f5e2667a2bebb7428b2baf227013288af366c357
Jingyu Liu <jyliu00@gmail.com>
2017-02-05 18:12:16 -0800

    update

--- ticker_list/rank1_goodvalue_trim.sh
+++ ticker_list/rank1_goodvalue_trim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cut -d$'\t' -f3,11 $1 | sed 'n; d' > zacks_rank1_goodvalue.txt
+echo "%sector=rank1_goodvalue" > zacks_rank1_goodvalue.txt
+cut -d$'\t' -f3,11 $1 | sed 'n; d' >> zacks_rank1_goodvalue.txt
 rm -f $1
