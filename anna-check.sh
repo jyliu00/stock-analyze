1f45a675e2817044305130a02d36554b67d5d210
Jingyu Liu <jyliu@fortinet.com>
2017-02-08 15:03:36 -0800

    update anna-check.sh

--- anna-check.sh
+++ anna-check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-printf "\n\x1b[33mStrong Break Out\x1b[0m:\n\n"
-
-anna -group=$1 check-strong-bo
-
 printf "\n\x1b[33mTrend Breakout\x1b[0m:\n\n"
 
 anna -group=$1 check-trend-bo
+
+printf "\n\x1b[33mStrong Break Out\x1b[0m:\n\n"
+
+anna -group=$1 check-strong-body-bo
 
 echo ""
