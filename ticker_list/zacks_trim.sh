b7762de5df822689fc4a28cfba9b990a5b8a5236
Jingyu Liu <jyliu00@gmail.com>
2017-02-11 11:42:39 -0800

    update zacks_1/2/3.txt

--- ticker_list/zacks_trim.sh
+++ ticker_list/zacks_trim.sh
@@ -2,6 +2,7 @@
 
 echo "%sector=$2" > zacks_$2.txt
 cut -d$'\t' -f3,4,7 $1 | sed 'n; d' >> zacks_$2.txt
+sed -i 's/[[:blank:]]\{1,\}$//' zacks_$2.txt
 sort -k 3 zacks_$2.txt > 1
 mv 1 zacks_$2.txt
 rm -f $1
