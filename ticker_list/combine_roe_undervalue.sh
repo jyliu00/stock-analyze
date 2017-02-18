ae603b16fa8f8cf68cb2c7585ca1314d80dde972
Jingyu Liu <jyliu00@gmail.com>
2017-02-18 09:05:10 -0800

    update zacks ticker list

--- ticker_list/combine_roe_undervalue.sh
+++ ticker_list/combine_roe_undervalue.sh
@@ -3,5 +3,7 @@
 cat zacks_rank1_roe.txt > 1
 cat zacks_rank1_undervalue.txt >> 1
 sort -k 3 1 > 2
-uniq 2 > zacks_rank1_roe_undervalue.txt
+uniq 2 > 1
+cut -d$'\t' -f1 1 > 2
+awk '{print $0 ","}' 2 > zacks_rank1_roe_undervalue.txt
 rm -f 1 2
