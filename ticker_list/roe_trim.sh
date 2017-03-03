335aba3e4b016f8f7dc68e6db9ac9c67f2bc5bb6
Jingyu Liu <jyliu00@gmail.com>
2017-03-03 07:06:03 -0800

    update zacks ticker list

--- ticker_list/roe_trim.sh
+++ ticker_list/roe_trim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "%sector=$2_roe" > zacks_$2_roe.txt
-cut -d$'\t' -f3,8,9 $1 | sed 'n; d' >> zacks_$2_roe.txt
+cut -d$'\t' -f3,4,7 $1 | sed 'n; d' >> zacks_$2_roe.txt
 sort -k 3 zacks_$2_roe.txt > 1
 mv 1 zacks_$2_roe.txt
 sed -i 's/[[:blank:]]\{1,\}$//' zacks_$2_roe.txt
