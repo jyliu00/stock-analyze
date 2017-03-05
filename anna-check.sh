1ddb3210a4a3ddf6a0afc583c44ffda464923ced
Jingyu Liu <jyliu00@gmail.com>
2017-03-05 13:02:48 -0800

    update anna-check.sh

--- anna-check.sh
+++ anna-check.sh
@@ -2,14 +2,14 @@
 
 printf "\n\x1b[33mTrend Breakout\x1b[0m:\n\n"
 
-anna -group=$1 check-trend-bo
+anna -group=$1 check-trend-bo $2
 
 printf "\n\x1b[33mStrong Break Out\x1b[0m:\n\n"
 
-anna -group=$1 check-strong-body-bo
+anna -group=$1 check-strong-body-bo $2
 
 printf "\n\x1b[33mSMA20d BreakOut\x1b[0m:\n\n"
 
-anna -group=$1 check-20d-bo
+anna -group=$1 check-20d-bo $2
 
 echo ""
