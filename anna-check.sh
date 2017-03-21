ec82b234e6e5b34739fc6d5c6c01c252532569b0
Jingyu Liu <jyliu00@gmail.com>
2017-03-20 21:31:57 -0700

    remove check-strong-20dup from anna-check.sh

--- anna-check.sh
+++ anna-check.sh
@@ -8,10 +8,6 @@ printf "\n\x1b[33mTrend Breakout\x1b[0m:\n\n"
 
 anna -group=$1 check-trend-bo $2
 
-printf "\n\x1b[33mStrong SMA20d Up\x1b[0m:\n\n"
-
-anna -group=$1 check-strong-20dup $2
-
 printf "\n\x1b[33mSMA20d BreakOut\x1b[0m:\n\n"
 
 anna -group=$1 check-20d-bo $2
