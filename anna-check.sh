2ab18fb7951aeb893647bce87147905cd1e6267f
Jingyu Liu <jyliu00@gmail.com>
2017-03-20 21:35:03 -0700

    add check-reverse-up into anna-check.sh

--- anna-check.sh
+++ anna-check.sh
@@ -4,6 +4,10 @@ printf "\n\x1b[33mStrong Break Out\x1b[0m:\n\n"
 
 anna -group=$1 check-strong-body-bo $2
 
+printf "\n\x1b[33mReverse Up\x1b[0m:\n\n"
+
+anna -group=$1 check-reverse-up $2
+
 printf "\n\x1b[33mTrend Breakout\x1b[0m:\n\n"
 
 anna -group=$1 check-trend-bo $2
