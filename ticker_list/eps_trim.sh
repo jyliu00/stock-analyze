880023452c738a609a19b872e99f0c11470a40c4
Jingyu Liu <jyliu@fortinet.com>
2017-05-31 14:44:10 -0700

    add zacks eps list

---
+++ ticker_list/eps_trim.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "%sector=$2" > zacks_$2.txt
+cut -d$'\t' -f3,9,10 $1 | sed 'n; d' >> zacks_$2.txt
+sed -i 's/[[:blank:]]\{1,\}$//' zacks_$2.txt
+sort -k 3 zacks_$2.txt > 1
+mv 1 zacks_$2.txt
+rm -f $1
